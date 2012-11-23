61d6f6eb47e90241bc19c54139138d188f9fd73a
stormuk <stormuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-23 08:17:24 +0000

    Bump to 1.0.5, fix documentation further
    git-svn-id: http://plugins.svn.wordpress.org/oauth-twitter-feed-for-developers/trunk@628892 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitter-feed-for-developers.php
+++ twitter-feed-for-developers.php
@@ -1,16 +1,12 @@
 <?php
 /*
- * Plugin Name: oAuth Twitter Feed for Developers
- * Description: Twitter API 1.1 compliant plugin that provides a function to get an array of tweets from the auth'd users Twitter feed for use in themes.
- * Contributors: stormuk, lgladdy
- * Donate link: http://www.stormconsultancy.co.uk/
- * Tags: twitter, oauth, feed, tweets
- * Requires at least: 3.4
- * Tested up to: 3.5
- * Stable tag: 1.0.4
- * Version: 1.0.4
- * License: MIT
- * License URI: http://opensource.org/licenses/MIT
+Plugin Name: oAuth Twitter Feed for Developers
+Description: Twitter API 1.1 compliant plugin that provides a function to get an array of tweets from the auth'd users Twitter feed for use in themes.
+Version: 1.0.5
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+Author: Storm Consultancy (Liam Gladdy)
+Author URI: http://www.stormconsultancy.co.uk
 */
 
 
